d0214d247dba921810e80e0fd4a84bd392c8c5fe
kphung29 <kphung29@gmail.com>
2020-04-28 14:44:46 -0400

    first commit

---
+++ src/main/java/com/cvshealth/loginformauthentication/LoginFormAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.cvshealth.loginformauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFormAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginFormAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cvshealth/loginformauthentication/LoginFormAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvshealth.loginformauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFormAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
